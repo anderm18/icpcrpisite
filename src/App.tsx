012cf8655da1774faca2e656f666b28eeec85dd2
Michael Anderson <michael.j.anderson@me.com>
2023-10-23 15:31:58 -0400

    Update spacing, new header, change info, cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const iconStyle = {
 } as const;
 
 function LeafletMapComponent() {
-  const position: [number, number] = [42.729907, -73.67663];
+  const position: [number, number] = [42.729202, -73.679311];
 
   const defaultIcon = L.icon({
     iconUrl: require('leaflet/dist/images/marker-icon.png'),
@@ -24,17 +24,18 @@ function LeafletMapComponent() {
 
   return (
     <Box sx={{margin: 'auto', width: {xs: '100%', sm: '80%', md: '50%'}}}>
-      <MapContainer center={position} zoom={15} style={{ width: '100%', height: '400px' }}>
+      <MapContainer center={position} zoom={16} style={{ width: '100%', height: '400px' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={position} icon={defaultIcon}>
           <Popup>
-            Rensselaer Union<br/><br/>
-            1761 15th St, <br/>
+            Darrin Communication Center<br/>Room 330<br/><br/>
+            110 8th Street,<br/>
             Troy, NY <br/>
-            12180
+            12180, <br/>
+            United States of America
           </Popup>
         </Marker>
       </MapContainer>
@@ -54,6 +55,14 @@ function App() {
             }}
           />
         </Typography>
+        <Typography variant='h5' align='center' sx={{fontWeight: 'bold', color: '#fde7e8'}}>
+          <Typewritter 
+            onInit={(typewriter) => {
+              typewriter.pauseFor(2000).typeString('October 29, 2023')
+              .start()
+            }}
+          />
+        </Typography>
       </Box>
       <Box sx={{backgroundColor: '#760A0D', minHeight: '50px'}}>
         <Typography align='center' sx={{fontWeight: 'bold', color: 'white', paddingTop: '13px', paddingBottom: '13px'}}>
@@ -61,51 +70,60 @@ function App() {
         </Typography>
       </Box>
       <Box sx={{backgroundColor: '#fde7e8', height: 'auto', paddingBottom: '50px'}}>
-        <Box>
-          <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
-            General Information
-          </Typography>
-          <Typography align='center' sx={{margin: '20px'}}>
-            Our site does NOT supply computers, participants will need to bring their own Laptops.<br/>
-            Printing services will be provided according to the conditions stated in the official ICPC rules regarding the number of pages printed per team, etc. Teams will need to bring a thumb drive or other storage device that they will use to put all materials they want to be printed on. There should be no other content on the drive other than materials that need to be printed.
-          </Typography>
-        </Box>
-        <Box>
-          <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
-            Directions
-          </Typography>
-          <Typography align='center' sx={{margin: '20px'}}>
-            The event will kickoff at the RPI Student Union<div style={{marginBottom:'20px'}}/>
-            <LeafletMapComponent/>
-            <br/>You may also want to follow the <a href='https://info.rpi.edu/visit/directions'>driving directions for RPI.</a>
-            Once you arrive, please pull into the Horseshoe-shaped dropoff area in the front of the Union. The kickoff event is held in the entry lobby of the Union.
-          </Typography>
-        </Box>
-        <Box>
-          <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
-            Tentative Schedule
-          </Typography>
-          <Typography align='center' sx={{margin: '20px'}}>
-            ~10:30 AM EST - Opening ceremony begins<br/>
-            11:00 AM EST - Contest begins<br/>
-            3:00 PM EST - Scoreboard freezes<br/>
-            4:00 PM EST - Contest finishes<br/>
-            Shortly after - Closing ceremony/results
-          </Typography>
-        </Box>
-        <Box>
-          <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
-            Locations
-          </Typography>
-          <Typography align='center' sx={{margin: '20px'}}>
-            Located in the Rensselaer Union Rooms 3418, 3202, 3502, 3602 and the Darrin Communication Center 232, 236, and 239, and the Voorhees Computing Center North 219 and South 208
-          </Typography>
+        <Box sx={{width: 'auto', maxWidth: '25cm', margin: 'auto'}}>
+          <Box>
+            <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
+              General Information
+            </Typography>
+            <Typography align='center' sx={{margin: '20px'}}>
+              Our site does NOT supply computers, participants will need to bring their own Laptops.<br/><br/>
+              Printing services will be provided according to the conditions stated in the official ICPC rules regarding the number of pages printed per team, etc. Teams will need to bring a thumb drive or other storage device that they will use to put all materials they want to be printed on. There should be no other content on the drive other than materials that need to be printed.
+            </Typography>
+          </Box>
+          <Box>
+            <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
+              Directions
+            </Typography>
+            <Typography align='center' sx={{margin: '20px'}}>
+              The event will kickoff at the Darring Communication Center (DCC)<div style={{marginBottom:'20px'}}/>
+              <LeafletMapComponent/>
+              <br/>You may also want to follow the <a href='https://info.rpi.edu/visit/directions'>driving directions for RPI.</a> Once you arrive, please proceed to Room 330 for the Opening Ceremony.
+            </Typography>
+          </Box>
+          <Box>
+            <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
+              Tentative Schedule
+            </Typography>
+            <Typography align='center' sx={{margin: '20px'}}>
+              ~10:30 AM EST - Opening ceremony begins<br/>
+              11:00 AM EST - Contest begins<br/>
+              3:00 PM EST - Scoreboard freezes<br/>
+              4:00 PM EST - Contest finishes<br/>
+              Shortly after - Closing ceremony/results
+            </Typography>
+          </Box>
+          <Box>
+            <Typography align='center' variant='h5' sx={{fontWeight: 'bold', paddingTop: '20px'}}>
+              Locations
+            </Typography>
+            <Typography align='center' sx={{margin: '20px'}}>
+              Contestant teams will be assigned to one of the following rooms to proceed to after the opening ceremony: <br/>
+              LOW 3045<br/>
+              LOW 3112<br/>
+              LOW 3130<br/>
+              LOW 4040<br/>
+              LOW 4034<br/>
+              DCC 235<br/>
+              DCC 236<br/>
+              DCC 239<br/>
+            </Typography>
+          </Box>
         </Box>
       </Box>
       <Box sx={{backgroundColor: '#760A0D', minHeight: '50px'}}>
         <Toolbar sx={{height: 'auto', color: 'white'}}>
-          <CardContent sx={{margin: 'auto'}}>
-              <Tooltip title={"we should probably put someones email here"} enterTouchDelay={0}>
+          <CardContent sx={{margin: 'auto', marginTop: '3px'}}>
+              <Tooltip title={"kuzmik2@rpi.edu"} enterTouchDelay={0}>
                   <EmailIcon sx={iconStyle} style={{ marginLeft: '10px', marginRight: '10px' }}/>
               </Tooltip>
               <a href={"https://github.com/anderm18/icpcrpisite"} style={{color: 'white', margin: '10px'}}><GitHubIcon sx={iconStyle}/></a>
